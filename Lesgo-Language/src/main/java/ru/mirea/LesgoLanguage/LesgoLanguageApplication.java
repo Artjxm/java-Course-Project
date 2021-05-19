f5f08a7caea28636642a8a8ceebe062baf3a90dd
ShadonPC <55762492+Shad0n@users.noreply.github.com>
2021-05-20 00:20:50 +0300

    fixed roots

---
+++ Lesgo-Language/src/main/java/ru/mirea/LesgoLanguage/LesgoLanguageApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.LesgoLanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesgoLanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesgoLanguageApplication.class, args);
+	}
+
+}
---
+++ Lesgo-Language/src/test/java/ru/mirea/LesgoLanguage/LesgoLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.LesgoLanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesgoLanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
